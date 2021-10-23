ac03f226a15d99be47a5ac622ba42b10199323fe
Beatriz <beatrizgsouza.bgs@gmail.com>
2021-10-22 21:10:47 -0300

    Atividades MySQL

---
+++ Atividade1.sql
@@ -0,0 +1,13 @@
+create table tb_funcionaries(
+id_funcionarie bigint auto_increment,
+nome varchar (100) not null,
+email varchar(50) not null, 
+cargo varchar(20) not null,
+salário float(12),
+primary key(id_funcionarie)
+							 );
+                             
+                             
+                            
+                            
+                            
