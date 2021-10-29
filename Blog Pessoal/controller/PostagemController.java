6d3dd7b3c8103c81b4a3e41ebb46a02141ce4b0a
Beatriz <beatrizgsouza.bgs@gmail.com>
2021-10-28 23:44:37 -0300

    Blog Pessoal || Spring

---
+++ Blog Pessoal/controller/PostagemController.java
@@ -0,0 +1,26 @@
+package org.generation.blogpessoal.controller;
+
+import java.util.List;
+
+import org.generation.blogpessoal.model.Postagem;
+import org.generation.blogpessoal.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+
+		
+	@Autowired			//Serviço de injeção de dependência
+	private PostagemRepository repository;
+		
+	public ResponseEntity<List<Postagem>> GetAll() {
+			return ResponseEntity.ok(repository.findAll());
+			
+	}
+}
